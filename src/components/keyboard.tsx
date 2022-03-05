c99bb50c794127804e360a5da5df1d2ef0a2f939
Irene Bioque <irenebioque@gmail.com>
2022-03-05 20:34:03 +0100

    format code

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -12,7 +12,11 @@ interface KeyboardProps {
   keysStates: KeyboardKey[];
 }
 
-export default function Keyboard({ keys, onKeyPressed, keysStates }: KeyboardProps) {
+export default function Keyboard({
+  keys,
+  onKeyPressed,
+  keysStates,
+}: KeyboardProps) {
   function handleInput(e: any) {
     onKeyPressed(e.target.textContent);
   }
@@ -27,70 +31,70 @@ export default function Keyboard({ keys, onKeyPressed, keysStates }: KeyboardPro
 
   return (
     <div className={styles.keyboardContainer}>
-        <div className={styles.keyboardRow}>
-            {Array.from(Array(10)).map((_, i) => (
-              <button
-                key={i}
-                className={
-                  keysStates[i].status === "correct"
-                    ? styles.correct_key
-                    : keysStates[i].status === "present"
-                    ? styles.present_key
-                    : keysStates[i].status === "absent"
-                    ? styles.absent
-                    : styles.unknown
-                }
-                onClick={handleInput}
-              >
-                {keys[i]}
-              </button>
-            ))}
-          </div>
-            <div className={styles.keyboardRow}>
-            {Array.from(Array(10)).map((_, i) => (
-              <button
-                key={i + 10}
-                className={
-                  keysStates[i + 10].status === "correct"
-                    ? styles.correct_key
-                    : keysStates[i + 10].status === "present"
-                    ? styles.present_key
-                    : keysStates[i + 10].status === "absent"
-                    ? styles.absent
-                    : styles.unknown
-                }
-                onClick={handleInput}
-              >
-                {keys[i + 10]}
-              </button>
-      ))}
-        </div>
       <div className={styles.keyboardRow}>
-              <button className={styles.enterKey} onClick={handleEnter}>
-        ENTER
+        {Array.from(Array(10)).map((_, i) => (
+          <button
+            key={i}
+            className={
+              keysStates[i].status === "correct"
+                ? styles.correct_key
+                : keysStates[i].status === "present"
+                ? styles.present_key
+                : keysStates[i].status === "absent"
+                ? styles.absent
+                : styles.unknown
+            }
+            onClick={handleInput}
+          >
+            {keys[i]}
+          </button>
+        ))}
+      </div>
+      <div className={styles.keyboardRow}>
+        {Array.from(Array(10)).map((_, i) => (
+          <button
+            key={i + 10}
+            className={
+              keysStates[i + 10].status === "correct"
+                ? styles.correct_key
+                : keysStates[i + 10].status === "present"
+                ? styles.present_key
+                : keysStates[i + 10].status === "absent"
+                ? styles.absent
+                : styles.unknown
+            }
+            onClick={handleInput}
+          >
+            {keys[i + 10]}
+          </button>
+        ))}
+      </div>
+      <div className={styles.keyboardRow}>
+        <button className={styles.enterKey} onClick={handleEnter}>
+          ENTER
         </button>
-       {Array.from(Array(7)).map((_, i) => (
-              <button
-                key={i + 20}
-                className={
-                  keysStates[i + 20].status === "correct"
-                    ? styles.correct_key
-                    : keysStates[i + 20].status === "present"
-                    ? styles.present_key
-                    : keysStates[i + 20].status === "absent"
-                    ? styles.absent
-                    : styles.unknown
-                }
-                onClick={handleInput}
-              >
-                {keys[i + 20]}
-              </button>
-      ))}
-  
+        {Array.from(Array(7)).map((_, i) => (
+          <button
+            key={i + 20}
+            className={
+              keysStates[i + 20].status === "correct"
+                ? styles.correct_key
+                : keysStates[i + 20].status === "present"
+                ? styles.present_key
+                : keysStates[i + 20].status === "absent"
+                ? styles.absent
+                : styles.unknown
+            }
+            onClick={handleInput}
+          >
+            {keys[i + 20]}
+          </button>
+        ))}
+
         <button className={styles.deleteKey} onClick={handleDelete}>
-          <RiDeleteBack2Line/>
+          <RiDeleteBack2Line />
         </button>
-        </div>
+      </div>
     </div>
   );
 }
