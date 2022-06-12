71344721d4677ca9cf9758b995d6a9e679b78dcf
Irene Bioque <irenebioque@gmail.com>
2022-06-12 21:28:57 +0200

    add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import App from "./App";
+
+import "@testing-library/jest-dom";
+import { getWordOfTheDay } from "./services/request";
+
+test("renders App", () => {
+  const history = createMemoryHistory();
+
+  render(
+    <Router location={history.location} navigator={history}>
+      <App />
+    </Router>
+  );
+  const Title = screen.getByText(/wordl/i);
+  expect(Title).toBeInTheDocument();
+});
+test("get words", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router location={history.location} navigator={history}>
+      <App />
+    </Router>
+  );
+
+  const RandomWord = getWordOfTheDay();
+  expect(RandomWord).toBeTruthy();
+  expect(RandomWord.length).toEqual(5);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Wordle from './components/wordle';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css"
+
+export const LocationDisplay = () => {
+  const location = useLocation();
+
+  return <div data-testid="location-display">{location.pathname}</div>;
+};
+
 function App() {
+  
   return (
     <Routes>
       <Route path="/" element={<Wordle />}>
